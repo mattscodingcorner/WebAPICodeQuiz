212dd9ff8d5b34e1e369db4b68a92cbb12627879
unknown <matt.j.velazquez@gmail.com>
2023-07-09 18:30:48 -0700

    updated logic and questions file

---
+++ assets/js/logic.js
@@ -0,0 +1,110 @@
+//Dom elements
+const startScreen = document.getElementById('start-screen');
+const questionsScreen = document.getElementById('questions');
+const endScreen = document.getElementById('end-screen');
+const timerElement = document.getElementById('time');
+const startButton = document.getElementById('start');
+const choicesContainer = document.getElementById('choices');
+const feedbackElement = document.getElementById('feedback');
+const initialsInput = document.getElementById('submit');
+const finalScoreElement = document.getElementById('final-score');
+const submitButton = document.getElementById('submit');
+
+
+//Quiz variables
+let currentQuestionIndex = 0;
+let timeLeft = 120;
+let timerInterval;
+let score = 0;
+
+// Event listeners
+
+startButton.addEventListener('click', startQuiz);
+submitButton.addEventListener('click', saveHighscore);
+
+function intializeQuiz() {
+    const startButton =document.getElementById('start');
+    startButton.addEventListener('click', startQuiz);
+}
+
+
+// Start the quiz
+function startQuiz() {
+    startScreen.classList.add('hide');
+    questionsScreen.classList.remove('hide');
+    intializeQuiz();
+    startTimer();
+    showQuesiton();
+}
+
+//timer
+function startTimer() {
+    timerInterval = setInterval(function () {
+    timeLeft--;
+    timerElement.textContent = timeLeft;
+
+    if (timerleft <= 0) {
+        clearInterval(timerInterval);
+        endQuiz();
+    }
+    }, 1000);
+}
+
+//Display questions and choices
+function showQuesiton() {
+    const currentQuestion = questions[currentQuestionIndex];
+    const questionTitleElement = document.getElementById('questions-title');
+    const choices = currentQuestions.choices;
+
+    questionTitleElement.textContent = currentQuestion.title;
+    choicesContainer.innerHTML = '';
+
+    choices.forEach(function (choice, index){
+        const choiceElement = document.createElement('button');
+        choiceElement.setAttribute('class', 'choice');
+        choiceElement.setAttribute('value', index);
+        choiceElement.textContent = index + 1 + '. ' + choice; 
+        choiceElement.addEventListener('click', handleChoice);
+        choicesContainer.appendChild(choiceElement);
+    });
+}
+
+//users choice selection
+function handleChoice () {
+    const selectedChoiceIndex = parseInt(this.value);
+
+    if (selectedChoiceIndex === questions[currentQuestionIndex].answer) {
+        //correct answer
+        score += 10;
+        feedbackElement.textContent = 'Correct!';
+        feedbackElement.classList.remove('hide');
+    } else {
+        //incorrect 
+        timeLeft -= 15;
+        feedbackElement.textContent = 'Incorrect!';
+        feedbackElement.classList.remove('hide');
+    }
+
+    //move to next question
+    currentQuestionIndex++;
+
+    //check if quis is over
+    if (currentQuestionIndex === questions.length) {
+        endQuiz();
+    } else {
+        showQuestion();
+    }
+}
+
+//end quiz
+function saveHighscore() {
+    const initials = initialsInput.value.trim();
+
+    if (initials !== '') {
+        const highscores = JSON.parse(localStorage.getItem('highscores')) || [];
+        const newScore = { initials, score };
+        highscores.push(newScore);
+        localStorage.setItem('highscores', JSON.stringify(highscores));
+        window.location.href = 'highscores.html'; //redirect to highscores page
+    }
+}
---
+++ assets/js/questions.js
@@ -0,0 +1,13 @@
+var questions = [
+    {
+        title: 'Question 1: An Array organizes data so that a related set of values can be easily sorted or searched.',
+        choices: ['True', 'False'],
+        answer: true
+    },
+    {
+        title: 'Question 2: The condition in an if/else statement is enclosed within ________.',
+        choices: ['Curly brackets', 'Square brackets', 'Parentheses', 'Angle brackets'],
+        answer: 2
+    }
+];
+
